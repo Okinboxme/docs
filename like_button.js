dc3e3ee47dfbe5042a1955b58b8b46517bde6bc4
okinboxme <Okinboxme@gmail.com>
2022-05-07 13:38:46 +0300

    Update like_button.js

--- like_button.js
+++ like_button.js
@@ -10,7 +10,12 @@ class LikeButton extends React.Component {
 
   render() {
     if (this.state.liked) {
-      return 'you clicked me ';
+      return <div>
+        <div>
+        
+        Many payroll activities are related to HR issues so the payroll and human resources departments must coordinate shared functions. This includes recruitment, salary increases, bonus payments, benefit deductions, vacation leaves and firing employees. The human resources department must be sensitive to the time devoted to payroll processing because as the champion of employee relations, they’ll face issues directly if paychecks aren’t processed correctly and on time. The payroll and HR departments are also privy to confidential employee data, including financial information, social security numbers and home addresses. The two departments must work together to ensure that this information doesn’t fall prey to unauthorized individuals or companies.
+          </div>
+        </div>;
     }
 
     return e(
